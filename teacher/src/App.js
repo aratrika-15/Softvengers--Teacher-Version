d0988baeb6e171b9dc4f119b8afb973108691677
Srishti <srishti.arora72@gmail.com>
2021-03-06 00:11:48 +0800

    added navbar

---
+++ teacher/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {NavigationBar} from './components/NavBar'; 
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar/> 
+    </div>
+    
+  );
+}
+
+export default App;
