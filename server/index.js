48ff104c2a8b11f5986240d87146e02bc6c4ee47
Aratrika Pal <dr.biplabpal@gmail.com>
2021-03-05 19:26:09 +0800

    added db connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
+//importing the necessary packages/modules
 const express = require('express');
 const mongoose = require('mongoose');
 const body_parser = require('body-parser');
 const cors = require('cors');
+
+//express app
+const app= express();
+
+//connecting to mongoDB and then listening for requests on port
+const dbConnection='';
+const PORT=process.env.PORT || 5000;
+mongoose.connect(dbConnection, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(result=>{
+    console.log(result);
+    app.listen(PORT);
+})
+.catch(err=>console.log(err));
+
+
