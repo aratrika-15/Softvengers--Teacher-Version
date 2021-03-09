a4bbf9207960bb669db8865b69c477e4616ad234
Aratrika Pal <dr.biplabpal@gmail.com>
2021-03-09 23:28:53 +0800

    added dotenv

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app= express();
 
 //connecting to mongoDB and then listening for requests on port
-const dbConnection='mongodb+srv://teacher_side:ssad_data@cluster0.8jxlk.mongodb.net/SSAD_Data?retryWrites=true&w=majority';
+const dbConnection=process.env.DBURL;
 const PORT=process.env.PORT || 5000;
 mongoose.connect(dbConnection, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(result=>{
