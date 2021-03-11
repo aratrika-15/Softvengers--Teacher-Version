105084d4ab7419a96cff483951fd35082bfb380f
saiteja1904 <reddysaiteja@gmail.com>
2021-03-11 23:34:06 +0800

    Added Leaderboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
 import {useState} from 'react'
-import CreateStudentAccount from './components/Views/CreateStudentAccount';
-import Students from './components/Views/Students';
+import CreateStudentAccount from './components/Views/CreateStudentAccount'
+import ViewLeaderboard from './components/Views/ViewLeaderboard'
+
 
 function App() {
   const [students, setStudents] = useState([
@@ -11,19 +12,21 @@ function App() {
         id:1,
         name: 'Saiteja',
         password: '123456',
-        email: 'reddysaiteja5@gmail.com'
+        email: 'reddysaiteja5@gmail.com',
+        rank:1,
+        score:25
     },
     {
         id:2,
         name: 'Kondreddy',
         password: '123456',
-        email: 'reddysaiteja5@gmail.com'
+        email: 'reddysaiteja5@gmail.com',
+        rank:3,
+        score:30
     }
 
 ])
-  
-
-  // const id =Math.floor(Math.random()*10000)+1
+// const id =Math.floor(Math.random()*10000)+1
   const onCreation = (student)=>{
     const id =Math.floor(Math.random()*10000)+1
     const newStudent = {id,...student}
@@ -33,8 +36,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
       <CreateStudentAccount onCreation = {onCreation}/>
+      <ViewLeaderboard/>
     </div>
   );
 }
