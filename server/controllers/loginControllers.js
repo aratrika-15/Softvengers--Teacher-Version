02359edf690e405592158ed0f5facf28593796a5
Aratrika Pal <dr.biplabpal@gmail.com>
2021-03-15 15:19:24 +0800

    added logic for login

--- server/controllers/loginControllers.js
+++ server/controllers/loginControllers.js
@@ -1,10 +1,44 @@
 /*
 * This file has controllers to deal with teacher login
 */
-const Teacher = require('../models/teacher');
 
-const teacherLogin=(req,res)=>{
+//imports 
+const Teacher = require('../models/teacher');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//setting constants
+const accessTokenSecret=process.env.TOKEN;
+const saltRounds=10;
+
+
+const teacherLogin=async(req,res)=>{
     console.log('teacher login in progress');
+    const {emailID,password}=req.body;
+      //validating the username and password
+      const emailExists = await Teacher.findOne({emailID: emailID});
+      if(emailExists)
+      {
+          const passwordCheck = await bcrypt.compare(password, emailExists.password);
+          if(passwordCheck)
+          {
+           //generating and returning the token
+           const token = jwt.sign({ email: emailExists.emailID,  id:emailExists._id , tutGp:emailExists.tutGrp}, accessTokenSecret);   
+           res.status(200).header('token', token).send(token);   
+          }
+          else
+          {
+              return res.status(404).send("Password is incorrect for the user");
+          }
+         
+      }
+      else
+      {
+          return res.status(404).send('Username does not exist');
+      }
+      
+  
+
 };
 
 const teacherRegister=(req,res)=>{
