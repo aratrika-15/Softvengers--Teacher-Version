dc8ee942564a13ca1bf40e5da217be7db353d539
Aratrika Pal <dr.biplabpal@gmail.com>
2021-03-16 12:45:38 +0800

    added hardcoding for question bank testing

--- server/controllers/questionControllers.js
+++ server/controllers/questionControllers.js
@@ -3,7 +3,21 @@
 */
 
 const questionList=(req,res)=>{
-
+    const qList=[
+        {
+            q_id:1,
+            body:"Whats ur name",
+            correct_option:"God",
+            wrong_options:["a","v","s"]
+        },
+        {
+            q_id:2,
+            body:"Whats ur age",
+            correct_option:"0",
+            wrong_options:["1","2","3"]
+        }
+    ]
+    res.send(qList);
 };
 
 const questionDetails=(req,res)=>{
