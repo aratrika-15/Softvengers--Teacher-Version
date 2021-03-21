70c7952a2cb6db90dea7387b208440c2d8957125
kush0511 <kush.g2100@gmail.com>
2021-03-21 16:27:48 +0800

    changes to stats and assignmentController

--- server/controllers/assignmentControllers.js
+++ server/controllers/assignmentControllers.js
@@ -2,7 +2,6 @@
 * controllers for assignment
 */
 const Assignment=require('../models/assignment');
-const AssignmentScore = require('../models/assignmentScore');
 const AssignmentQn = require('../models/assignmentQuestion');
 const Student = require('../models/student');
 const schemas = require('../models/assignmentScore');
