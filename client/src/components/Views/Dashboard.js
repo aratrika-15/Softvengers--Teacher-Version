03e9e337f0902b1561bbf49a5aeaf9d7d2167ee7
saiteja1904 <reddysaiteja@gmail.com>
2021-04-01 15:12:12 +0800

    stats and slice fixed

--- client/src/components/Views/Dashboard.js
+++ client/src/components/Views/Dashboard.js
@@ -1,5 +1,27 @@
 import {useState,useEffect} from 'react'
+import Paper from '@material-ui/core/Paper';
+import {
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+import CustomPieChart from "../CustomPieChart";
+import { PieChart } from 'react-minimal-pie-chart';
+import {CanvasJSChart} from 'canvasjs-react-charts'
+
+import { Animation } from '@devexpress/dx-react-chart';
 const Dashboard = () => {
+  const data = [
+    { year: '1950', population: 2.525 },
+    { year: '1960', population: 3.018 },
+    { year: '1970', population: 3.682 },
+    { year: '1980', population: 4.440 },
+    { year: '1990', population: 5.310 },
+    { year: '2000', population: 6.127 },
+    { year: '2010', population: 6.930 },
+  ];
   const [showForm, setShowForm] = useState(false);
   const [indv, setinv] = useState([])
   const [grps, setgrps] = useState([])
@@ -44,8 +66,62 @@ const Dashboard = () => {
   const Form = () => {
     setShowForm(!showForm)
   }
+  const propsData = [
+    //TODO: Hardcoded values for now, extract from DB
+    { title: 'Planning and Defining', value: 80, color: '#ff9800'},
+    { title: 'Design', value: 25, color: '#f44336'},
+    { title: 'Implementation', value: 40, color: '#4caf50'},
+    { title: 'Testing and Maintainance', value: 60, color: '#00acc1'},
+  ]
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", // "light1", "dark1", "dark2"
+    title:{
+      text: "Bounce Rate by Week of Year"
+    },
+    axisY: {
+      title: "Bounce Rate",
+      suffix: "%"
+    },
+    axisX: {
+      title: "Week of Year",
+      prefix: "W",
+      interval: 2
+    },
+    data: [{
+      type: "line",
+      toolTipContent: "Week {x}: {y}%",
+      dataPoints: [
+        { x: 1, y: 64 },
+        { x: 2, y: 61 },
+        { x: 3, y: 64 },
+        { x: 4, y: 62 },
+        { x: 5, y: 64 },
+        { x: 6, y: 60 },
+        { x: 7, y: 58 },
+        { x: 8, y: 59 },
+        { x: 9, y: 53 },
+        { x: 10, y: 54 },
+        { x: 11, y: 61 },
+        { x: 12, y: 60 },
+        { x: 13, y: 55 },
+        { x: 14, y: 60 },
+        { x: 15, y: 56 },
+        { x: 16, y: 60 },
+        { x: 17, y: 59.5 },
+        { x: 18, y: 63 },
+        { x: 19, y: 58 },
+        { x: 20, y: 54 },
+        { x: 21, y: 59 },
+        { x: 22, y: 64 },
+        { x: 23, y: 59 }
+      ]
+    }]
+  }
 
   return (
+    <div>
     <div className = 'button' >
       <input  type = 'submit' value = 'Group'className ='btn2 btn2-block' onClick={Form2}/>
       <input type = 'submit' value = 'Individual' className ='btn2 btn2-block' onClick={Form}/>
@@ -61,9 +137,63 @@ const Dashboard = () => {
         
           
       )}
-      
-       
-      
+     </div>
+     <div >
+     <div>
+     <Paper className = 'paper'>
+        <Chart
+          data={data}
+        >
+          <ArgumentAxis />
+          <ValueAxis max={7} />
+
+          <BarSeries
+            valueField="population"
+            argumentField="year"
+          />
+          <Title text="World population" />
+          <Animation />
+        </Chart>
+      </Paper>
+      </div>
+      <div>
+      <Paper className = 'paper'>
+        <Chart
+          data={data}
+        >
+          <ArgumentAxis />
+          <ValueAxis max={7} />
+
+          <BarSeries
+            valueField="population"
+            argumentField="year"
+          />
+          <Title text="World population" />
+          <Animation />
+        </Chart>
+      </Paper>
+      </div>
+      <div>
+      <Paper className = 'paper'>
+        <PieChart 
+            radius={PieChart.defaultProps.radius-10}
+              viewBoxSize={[100,100]}
+              data={propsData}
+              segmentsShift={(index) => (index === 0 ? 1 : 0.5)}
+              label={({ dataEntry }) => dataEntry.title }
+              labelStyle={{
+                fontSize: '3px',
+                labelPosition: 30
+              }}
+            />
+      </Paper>
+      <Paper className = 'paper'>
+      <CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+      </Paper>
+      </div>
+            </div>
     </div>
   )
 }
