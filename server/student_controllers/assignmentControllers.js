f1a6a56b02fd9ddb61583cd90eba630855792274
Atul Acharya <61643403+Atul-Acharya-17@users.noreply.github.com>
2021-04-03 02:11:37 +0800

    Update assignmentControllers.js

--- server/student_controllers/assignmentControllers.js
+++ server/student_controllers/assignmentControllers.js
@@ -79,7 +79,7 @@ const getassignmentList=async (req,res)=>{
 const getAssignmentQuestions = async (req,res) =>{
     try{
     const questions = await AssignmentQn.find({
-        _id:req.query.assignmentID
+        "assignmentID":req.query.assignmentID
         
     });
     console.log(questions);
