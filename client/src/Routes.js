e80b028410f176a2b52fafb8c322becea441b4ec
Srishti <srishti.arora72@gmail.com>
2021-04-02 16:38:15 +0800

    minor bug

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import CreateStudentAccount from './components/Views/CreateStudentAccount'
 import ViewLeaderboard from './components/Views/ViewLeaderboard'
-import ViewAssignment from './components/Views/OldViewAssignments'
+
 import Dashboard from './components/Views/Dashboard'
 import Questionbank from './components/Views/ViewQuestionbank'
 import CreateAssignment  from './components/Views/CreateAssignment'
